dc0ea4a8fbc16f5a62e83e925666b6e3a5429e02
alim1496 <alim1496@gmail.com>
2022-01-04 01:35:35 +0600

    add login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { config } from "../utils/api";
 
-const Login = () => (
-    <div>Hello from login</div>
-);
+const Login = () => {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        document.querySelector("#main_navbar").classList.add("d-none");
+    }, []);
+
+    const login = (e) => {
+        e.preventDefault();
+        const { target } = e;
+        if (name === "" || password === "") return;
+        target.classList.add("loading");
+        const data = {
+            username: name,
+            password,
+        };
+        setLoading(true);
+        setError(false);
+        axios
+            .post("/api/users/login", data, config)
+            .then(({ data: { token } }) => {
+                Cookies.set("token", token);
+                setLoading(false);
+                setError(false);
+            })
+            .catch(({response: { data }}) => {
+                setLoading(false);
+                setError(true);
+            });
+    };
+
+    return (
+        <div className="container grid-xs login-container text-center">
+            <h3 className="text-bold">Admin Login</h3>
+            <form>
+                <div className="form-group">
+                    <input
+                        className="form-input"
+                        type="text"
+                        id="username"
+                        placeholder="username"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                    <input
+                        className="form-input"
+                        type="password"
+                        id="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                {loading
+                    ? <div className="loading"></div> 
+                    : <input type="submit" value="Login" className="btn btn-primary text-bold padded" onClick={login} />}
+                {error && <p className="text-error">Does not have admin access</p>}
+            </form>
+        </div>
+    );
+};
 
 export default Login;
